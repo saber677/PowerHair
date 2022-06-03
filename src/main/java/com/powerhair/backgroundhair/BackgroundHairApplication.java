efca25cdd0e81339cbee48d436f471f0674941c4
luqiqi <luqiqi@cloudbility.com>
2022-06-03 18:04:05 +0800

    test

---
+++ src/main/java/com/powerhair/backgroundhair/BackgroundHairApplication.java
@@ -0,0 +1,13 @@
+package com.powerhair.backgroundhair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackgroundHairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackgroundHairApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/powerhair/backgroundhair/BackgroundHairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerhair.backgroundhair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackgroundHairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
