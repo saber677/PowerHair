29a4854ae236a13c32091ef638beebf9a00100d1
luqiqi <luqiqi@cloudbility.com>
2022-08-27 18:28:23 +0800

    add 拦截器log

--- src/main/java/com/powerhair/backgroundhair/intercepter/LoginInterceptor.java
+++ src/main/java/com/powerhair/backgroundhair/intercepter/LoginInterceptor.java
@@ -1,6 +1,9 @@
 package com.powerhair.backgroundhair.intercepter;
 
+import com.powerhair.backgroundhair.module.console.service.impl.ConsoleAccountServiceImpl;
 import com.powerhair.backgroundhair.utils.constant.ConstantSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,10 +12,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.Objects;
 
 public class LoginInterceptor implements HandlerInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Boolean isLogin = (Boolean) request.getSession().getAttribute(ConstantSession.USER_LOGIN_STATUS);
+        logger.info("开始拦截,是否通过拦截:{}",(Objects.isNull(isLogin) && isLogin));
         return (!Objects.isNull(isLogin) && isLogin) ? true : false;
     }
 
---
+++ src/main/java/com/powerhair/backgroundhair/module/store/controller/StoreController.java
@@ -0,0 +1,8 @@
+package com.powerhair.backgroundhair.module.store.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/store")
+public class StoreController {
+
+}
